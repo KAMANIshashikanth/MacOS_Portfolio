8e922dd5ee7eb3b6a21ec1cb4998502d039cede6
Shashikanth <kamanishashikanth@gmail.com>
2025-12-15 00:44:07 +0530

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import Navbar from "./components/Navbar";
+import { Navbar, Welcome } from "#components";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   );
 };
 
 export default App;
-
-
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/KAMANIshashikanth/MacOS_Portfolio.git
-// git push -u origin main
