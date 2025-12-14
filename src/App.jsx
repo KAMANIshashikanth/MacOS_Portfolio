015bae4d20046ce938b79be17c994f85777c44c0
Shashikanth <kamanishashikanth@gmail.com>
2025-12-14 23:19:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+
+export default App;
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/KAMANIshashikanth/MacOS_Portfolio.git
+// git push -u origin main
